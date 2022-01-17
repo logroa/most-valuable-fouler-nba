e7e8dbbef823c8bcc68f590dfb5925a40916da87
logroa <logroa@umich.edu>
2022-01-17 15:25:39 -0500

    fts and made fts

--- foul-agg.py
+++ foul-agg.py
@@ -14,4 +14,14 @@ fouls_and_freethrows = playbyplay.loc[(playbyplay['FoulType'].notnull()) | (play
                                        'HomeScore', 'FoulType', 'Fouler', 'Fouled', 'FreeThrowShooter', 'FreeThrowOutcome',
                                        'FreeThrowNum']]
 instances = fouls_and_freethrows.groupby(['URL', 'Quarter', 'SecLeft'])
+#fouls
 fouls = instances.first()
+#made free throws
+fouls_and_freethrows['made_ft'] = np.where(fouls_and_freethrows['FreeThrowOutcome'] == 'make', 1, 0)
+mft = fouls_and_freethrows[['URL', 'Quarter', 'SecLeft', 'made_ft']]
+mft1 = mft.groupby(['URL', 'Quarter', 'SecLeft']).sum()
+#num free throws
+num_ft = fouls_and_freethrows.groupby(['URL', 'Quarter', 'SecLeft']).size() - 1
+#
+new_df = pd.merge(fouls, pd.merge(num_ft.rename('num_ft'), mft1, on=['URL', 'Quarter', 'SecLeft']), on=['URL', 'Quarter', 'SecLeft'])
+total = new_df.rename(columns={'0': 'num_ft'})
