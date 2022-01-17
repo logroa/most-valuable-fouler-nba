c916950bbb36fce79ee2e253f6e78b721c670981
logroa <logroa@umich.edu>
2022-01-17 02:13:00 -0500

    progress creating single df

--- foul-agg.py
+++ foul-agg.py
@@ -9,4 +9,7 @@ playbyplay = pd.read_csv('NBA_PBP_2015-16.csv')
 #no playoff data
 playbyplay = playbyplay.loc[playbyplay['GameType'] == 'regular']
 #need just fouls and freethrows
-fouls_and_freethrows = playbyplay.loc[(playbyplay['FoulType'].notnull()) | (playbyplay['FreeThrowOutcome'].notnull())]
+fouls_and_freethrows = playbyplay.loc[(playbyplay['FoulType'].notnull()) | (playbyplay['FreeThrowOutcome'].notnull()),
+                                      ['Quarter', 'SecLeft', 'AwayTeam', 'AwayPlay', 'AwayScore', 'HomeTeam', 'HomePlay',
+                                       'HomeScore', 'FoulType', 'Fouler', 'Fouled', 'FreeThrowShooter', 'FreeThrowOutcome',
+                                       'FreeThrowNum']]
