a3b1b22cf09918d98b38bc1fdf4bea6d746cba29
logroa <logroa@umich.edu>
2022-01-17 01:49:10 -0500

    beginning data manipulation

---
+++ foul-agg.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+import pandas as pd
+import csv
+import math
+
+#read in data
+playbyplay = pd.read_csv('NBA_PBP_2015-16.csv')
+#no playoff data
+playbyplay = playbyplay.loc[playbyplay['GameType'] == 'regular']
+#need just fouls and freethrows
+fouls_and_freethrows = playbyplay.loc[(playbyplay['FoulType'].notnull()) | (playbyplay['FreeThrowOutcome'].notnull())]
